af66dab208e3fc5432f7cd17e6258fde802e5947
Stepan Karpov <stepan-karpov-2025@mail.ru>
2025-11-04 15:21:03 +0300

    fix

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -14,7 +14,7 @@ from core.models import Base
 # access to the values within the .ini file in use.
 config = context.config
 
-# Interpret the config file for Python logging.
+# Interpret the test_config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
@@ -25,9 +25,9 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
+# other values from the test_config, defined by the needs of env.py,
 # can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
+# my_important_option = test_config.get_main_option("my_important_option")
 # ... etc.
 config.set_main_option("sqlalchemy.url", str(settings.db.url))
 
